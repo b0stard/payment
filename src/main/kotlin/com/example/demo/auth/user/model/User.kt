5356c74bbcb60a93d4d474ef1b4d3eca3cc95967
dippe <sostanin378@gmail.com>
2026-06-03 19:37:21 +0300

    pay

--- src/main/kotlin/com/example/demo/auth/user/model/User.kt
+++ src/main/kotlin/com/example/demo/auth/user/model/User.kt
@@ -3,25 +3,25 @@ package com.example.demo.auth.user.model
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import java.time.Instant
 import java.util.UUID
+
+
 
 @Entity
 @Table(name = "users")
 class User(
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.UUID)
     var id: UUID? = null,
 
     @Column(nullable = false, unique = true)
-    var email: String,
+    var email: String = "",
 
     @Column(nullable = false)
-    var password: String,
+    var password: String = ""
 
-    @Column(nullable = false)
-    var createdAt: Instant = Instant.now()
 )
